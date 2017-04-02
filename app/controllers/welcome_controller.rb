975abc45c57b7132ae2ce1d00c4c337aa5cbe2c7
riverhome <liushuijiaxiang@gmail.com>
2017-04-02 16:38:01 +0800

    create groups index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,7 @@
 class WelcomeController < ApplicationController
 
+  def index
+    flash[:warning] = "this is warning"
+  end
+
 end
---
+++ test/models/froup_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FroupTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
